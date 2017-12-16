3c7046b5e79fd029de6b121a3876ab8afa4cfeba
pigchick3n <League@AdministorsiMac.attlocal.net>
2017-12-15 17:00:15 -0800

    Drum Kit, Body Part Quiz (IP)

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -15,6 +15,8 @@ import javax.swing.JPanel;
 public class DrumKit extends MouseAdapter {
 
     JLabel drumLabelWithImage;
+    JLabel drumLabelWithImage2;
+    JLabel drumLabelWithImage3;
 
     public static void main(String[] args) throws Exception {
    	 new DrumKit().getGoing();
@@ -36,17 +38,24 @@ public class DrumKit extends MouseAdapter {
    	 // 7. Download an image of a drum from the Internet. Drop it into your Eclipse project under "default package".
    	 // 8. Put the name of your image in a String variable.
     String drumimage1 = "drum 1.jpg";
+    String drumimage2 = "cymbal.jpeg";
+    String drumimage3 = "bass drum.jpeg";
    	 // 9. Edit the next line to use your String variable
  drumLabelWithImage = createLabelImage(drumimage1);
-   	 
+ drumLabelWithImage2 = createLabelImage(drumimage2);
+ drumLabelWithImage3 = createLabelImage(drumimage3);
    	 // 10. Add the image to the panel
     panel.add(drumLabelWithImage);
+    panel.add(drumLabelWithImage2);
+    panel.add(drumLabelWithImage3);
    	 // 11. Set the layout of the panel to "new GridLayout()"
     panel.setLayout(new GridLayout());
    	 // 12. call the pack() method on the frame
    	 frame.pack();
    	 // 13. add a mouse listener to drumLabelWithImage.
    	drumLabelWithImage.addMouseListener(this);
+	drumLabelWithImage2.addMouseListener(this);
+	drumLabelWithImage3.addMouseListener(this);
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -55,9 +64,15 @@ public class DrumKit extends MouseAdapter {
    	 JLabel drumClicked = (JLabel) e.getSource();
    	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
    	 // 16. If they clicked on the drumImage...
-
+   	 if(drumClicked == drumLabelWithImage) {
+   		 playSound("snaredrum.wav"); 
+   	 }else if(drumClicked == drumLabelWithImage2){
+   		 playSound("cymbal.wav"); 
+   	 }else if(drumClicked == drumLabelWithImage3) {
+   		 playSound("bassdrum.wav");
+   	 }
    		 // 17. ...use the playSound method to play a drum sound.
-   	 playSound("snaredrum.wav");
+   
    	 // 18. Add more images to make a drumkit. Remember to add a mouse listener to each one.
     }
 
