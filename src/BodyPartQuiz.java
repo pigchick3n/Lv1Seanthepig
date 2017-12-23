b49c26a0f862635d59e68fd7ea5ba2a20914f01d
pigchick3n <League@AdministorsiMac.attlocal.net>
2017-12-22 16:59:24 -0800

    Body Part Quiz, Dueling Buttons

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -9,22 +9,53 @@ import javax.swing.JOptionPane;
 
 public class BodyPartQuiz {
 
-	// Get 3 celebrity photos from the internet, save them to your computer, and fill
-	// in their paths here. To get the path, right click on the image, and copy its Location.
-	String firstImage = "/pathtofile/example.jpg";
-	String secondImage = "/pathtofile/example2.jpg";
-	String thirdImage = "/pathtofile/example3.jpg";
+	// Get 3 celebrity photos from the internet, save them to your computer, and
+	// fill
+	// in their paths here. To get the path, right click on the image, and copy its
+	// Location.
+	String firstImage = "/Users/League/Desktop/Lv1Seanthepig/src/giratina.jpg";
+	String secondImage = "/Users/League/Desktop/Lv1Seanthepig/src/pokemon-snorlax.jpg";
+	String thirdImage = "/Users/League/Desktop/Lv1Seanthepig/src/empoleon.jpeg";
 
 	Frame window = new Frame();
-	
+
 	private void startQuiz() {
 		// Set the size of the window so that you can only see part of the image.
-			
+		window.setSize(250, 250);
 		// Make an int variable to hold the score.
-
+		int score = 0;
 		// Ask the user who this person is and store their answer
-
+		String answer = JOptionPane.showInputDialog("What pokemon is this?");
 		// If they got the answer right:
+		if (answer.equals("Giratina")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
+		showNextImage();
+		String answer2 = JOptionPane.showInputDialog("What pokemon is this?");
+		// If they got the answer right:
+		if (answer.equals("Snorlax")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
+		showNextImage();
+		String answer3 = JOptionPane.showInputDialog("What pokemon is this?");
+		// If they got the answer right:
+		if (answer.equals("Empoleon")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
+		JOptionPane.showMessageDialog(null, "Your score is "+ score);
+	}
 		// -- Tell them they are correct.
 		// -- Increase the score by 1
 
@@ -34,7 +65,7 @@ public class BodyPartQuiz {
 		// Use the method below to show the next image
 		// Show the score
 		// .... repeat for all your images.....
-	}
+	
 
 	public void showNextImage() {
 		window.removeAll();
@@ -79,5 +110,5 @@ public class BodyPartQuiz {
 		window.dispose();
 		return new JLabel();
 	}
-}
 
+}
