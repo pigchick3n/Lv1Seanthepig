cc11d9d1b8259498d578f51467c008d624a35a9b
pigchick3n <league@WTS10.attlocal.net>
2018-03-02 16:59:42 -0800

    athlete

--- src/Athlete.java
+++ src/Athlete.java
@@ -1,4 +1,16 @@
 
 public class Athlete {
-
+//static for variables means just 1
+	int speed;
+	String name;
+	static int count;
+	static String location = "San Diego";
+	static int time = 500;
+	
+	//constructer (looks like class but without class)
+	public Athlete(int speed, String name){
+		this.name = name;
+		this.speed = speed;
+		count++;
+	}
 }
