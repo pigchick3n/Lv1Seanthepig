6fff1b34557a2a9557797dcdc9d4d35a07452321
League <league@WTS10.attlocal.net>
2018-03-16 16:57:47 -0700

    cuteness TV . jukebox ip

--- src/CutenessTV.java
+++ src/CutenessTV.java
@@ -1,15 +1,34 @@
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class CutenessTV {
+public class CutenessTV implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton vid1 = new JButton("Video 1");
 	JButton vid2 = new JButton("Video 2");
 	JButton vid3 = new JButton("Video 3");
-	frame.setvisible
-	frame.add
+
+	public static void main(String[] args) {
+		CutenessTV ctv = new CutenessTV();
+		ctv.run();
+	}
+	public void run(){
+		frame.add(panel);
+		panel.add(vid1);
+		panel.add(vid2);
+		panel.add(vid3);
+		frame.setVisible(true);
+		frame.pack();
+		vid1.addActionListener(this);
+		vid2.addActionListener(this);
+		vid3.addActionListener(this);
+	}
 void showDucks() {
 		playVideo("Drirjl5K9Yk");
 	}
@@ -24,11 +43,33 @@ void showDucks() {
 
 	void playVideo(String videoID) {
 		try {
-	URI uri = new URI("https://www.youtube.com/v/" + videoID + "?autoplay=1");
+	URI uri = new URI("https://www.youtube.com/watch?v=" + videoID);
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if(buttonPressed==vid1) {
+			showDucks();
+		}else if(buttonPressed==vid2) {
+			showFrog();
+		}else if(buttonPressed==vid3){
+			showFluffyUnicorns();
+		}
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
