e4772ce0605d5b758ab8c64a46da79003a36b15a
pigchick3n <League@AdministorsiMac.attlocal.net>
2018-05-11 16:59:24 -0700

    Candyman, Netflix ip

--- src/Candyman.java
+++ src/Candyman.java
@@ -1,8 +1,12 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -13,37 +17,65 @@ import javax.swing.JPanel;
 public class Candyman implements ActionListener {
 	JFrame frame = new JFrame();
 	JButton button = new JButton("Candyman");
-	String scarypic = "https://static.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/51ce61f9e4b0d911b44a8353/1303420352094/1000w/Bert2mail.jpg"; 
+	String scarypic = "https://static.squarespace.com/static/51b3dc8ee4b051b96ceb10de/51ce6099e4b0d911b4489b79/51ce61f9e4b0d911b44a8353/1303420352094/1000w/Bert2mail.jpg";
+	int clicks = 0;
+
 	public static void main(String[] args) {
 		Candyman c = new Candyman();
 		c.run();
 	}
-	void run(){
+
+	void run() {
 		frame.add(button);
 		button.addActionListener(this);
 		frame.pack();
 		frame.setVisible(true);
 	}
-	
 
-
-
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-private void showPictureFromTheInternet(String imageUrl) {
-	try {
-		URL url = new URL(imageUrl);
-		Icon icon = new ImageIcon(url);
-		JLabel imageLabel = new JLabel(icon);
-		JFrame frame = new JFrame();
-		frame.add(imageLabel);
-		frame.setVisible(true);
-		frame.pack();
-	} catch (MalformedURLException e) {
-		e.printStackTrace();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		clicks += 1;
+		
+		if (clicks == 5) {
+			playSound(1);
+			showPictureFromTheInternet(scarypic);
+		}else {
+			playSound(0);
+		}
 	}
-}
+
+	private void showPictureFromTheInternet(String imageUrl) {
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static final int CREEPY = 0;
+	static final int SCREAM = 1;
+
+	public static void playSound(int whichSound) {
+		File soundFile = null;
+		if (whichSound == CREEPY)
+			soundFile = new File("/Users/League/Desktop/Lv1Seanthepig/src/creepysound.wav");
+		else if (whichSound == SCREAM)
+			soundFile = new File("/Users/League/Desktop/Lv1Seanthepig/src/scream.wav");
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
